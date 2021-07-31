0b60e826f1a5b4842b69895202149c7bfebb443d
Monali <monalidoshi9@gmail.com>
2021-07-31 12:14:22 +0530

    Add linting using eslint and prettier

--- src/user/components/header.js
+++ src/user/components/header.js
@@ -7,78 +7,72 @@ import {
   IconButton,
   Drawer,
   Link,
-  MenuItem,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import React, { useState, useEffect } from "react";
-import { Link as RouterLink } from "react-router-dom";
-import { GoogleLoginButton } from "react-social-login-buttons";
-import firebase from "firebase/app";
-import "firebase/auth";
-import { useHistory } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
-import Terms from "../views/login";
+  MenuItem
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import React, { useState, useEffect } from 'react'
+import { Link as RouterLink, useHistory } from 'react-router-dom'
+import { GoogleLoginButton } from 'react-social-login-buttons'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 const headersData = [
   {
-    label: "Home",
-    href: "/listings",
+    label: 'Home',
+    href: '/listings'
   },
   {
-    label: "About us",
-    href: "/account",
+    label: 'About us',
+    href: '/account'
   },
   {
-    label: "Contact us",
-    href: "/logout",
-  },
-];
+    label: 'Contact us',
+    href: '/logout'
+  }
+]
 
 const useStyles = makeStyles(() => ({
   header: {
-    backgroundColor: "#616161",
-    paddingRight: "79px",
+    backgroundColor: '#616161',
+    paddingRight: '79px',
 
-    paddingLeft: "118px",
-    "@media (max-width: 900px)": {
-      paddingLeft: 0,
-    },
+    paddingLeft: '118px',
+    '@media (max-width: 900px)': {
+      paddingLeft: 0
+    }
   },
   logo: {
-    fontFamily: "Work Sans, sans-serif",
+    fontFamily: 'Work Sans, sans-serif',
     fontWeight: 600,
-    color: "#FFFEFE",
-    textAlign: "left",
+    color: '#FFFEFE',
+    textAlign: 'left'
   },
   menuButton: {
-    fontFamily: "Open Sans, sans-serif",
+    fontFamily: 'Open Sans, sans-serif',
     fontWeight: 700,
-    size: "18px",
-    marginLeft: "38px",
+    size: '18px',
+    marginLeft: '38px'
   },
   toolbar: {
-    display: "flex",
-    justifyContent: "space-between",
+    display: 'flex',
+    justifyContent: 'space-between'
   },
   drawerContainer: {
-    padding: "20px 30px",
+    padding: '20px 30px'
   },
   button: {
-    justifyContent: "center",
-    display: "grid",
-    padding: "18px",
+    justifyContent: 'center',
+    display: 'grid',
+    padding: '18px'
   },
   buttonmargin: {
-    marginTop: 10,
-  },
-}));
+    marginTop: 10
+  }
+}))
 
-export default function Header() {
+export default function Header () {
   const {
     header,
     logo,
@@ -86,50 +80,50 @@ export default function Header() {
     toolbar,
     drawerContainer,
     button,
-    buttonmargin,
-  } = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const history = useHistory();
+    buttonmargin
+  } = useStyles()
+  const [open, setOpen] = React.useState(false)
+  const history = useHistory()
   const [state, setState] = useState({
     mobileView: false,
-    drawerOpen: false,
-  });
+    drawerOpen: false
+  })
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [openDialogName, setOpenDialog] = React.useState(null);
+  // const [anchorEl, setAnchorEl] = React.useState(null);
+  // const [openDialogName, setOpenDialog] = React.useState(null);
 
-  const { mobileView, drawerOpen } = state;
+  const { mobileView, drawerOpen } = state
 
   useEffect(() => {
     const setResponsiveness = () => {
       return window.innerWidth < 900
-        ? setState((prevState) => ({ ...prevState, mobileView: true }))
-        : setState((prevState) => ({ ...prevState, mobileView: false }));
-    };
+        ? setState(prevState => ({ ...prevState, mobileView: true }))
+        : setState(prevState => ({ ...prevState, mobileView: false }))
+    }
 
-    setResponsiveness();
+    setResponsiveness()
 
-    window.addEventListener("resize", () => setResponsiveness());
+    window.addEventListener('resize', () => setResponsiveness())
 
     return () => {
-      window.removeEventListener("resize", () => setResponsiveness());
-    };
-  }, []);
+      window.removeEventListener('resize', () => setResponsiveness())
+    }
+  }, [])
 
   const LoginPopup = () => {
-    setOpen(true);
-  };
+    setOpen(true)
+  }
 
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   const handleLogin = async () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    await firebase.auth().signInWithPopup(provider);
-    setOpen(false);
-    history.push("/");
-  };
+    const provider = new firebase.auth.GoogleAuthProvider()
+    await firebase.auth().signInWithPopup(provider)
+    setOpen(false)
+    history.push('/')
+  }
 
   const displayDesktop = () => {
     return (
@@ -139,74 +133,70 @@ export default function Header() {
           {getMenuButtons()}
           <Button
             style={{
-              color: "inherit",
-              fontFamily: "Open Sans, sans-serif",
+              color: 'inherit',
+              fontFamily: 'Open Sans, sans-serif',
               fontWeight: 700,
-              size: "18px",
-              marginLeft: "38px",
+              size: '18px',
+              marginLeft: '38px'
             }}
-            onClick={LoginPopup}
-          >
-            {"Login"}
+            onClick={LoginPopup}>
+            {'Login'}
           </Button>
         </div>
       </Toolbar>
-    );
-  };
+    )
+  }
 
   const displayMobile = () => {
     const handleDrawerOpen = () =>
-      setState((prevState) => ({ ...prevState, drawerOpen: true }));
+      setState(prevState => ({ ...prevState, drawerOpen: true }))
     const handleDrawerClose = () =>
-      setState((prevState) => ({ ...prevState, drawerOpen: false }));
+      setState(prevState => ({ ...prevState, drawerOpen: false }))
 
     return (
       <Toolbar>
         <IconButton
           {...{
-            edge: "start",
-            color: "inherit",
-            "aria-label": "menu",
-            "aria-haspopup": "true",
-            onClick: handleDrawerOpen,
-          }}
-        >
+            edge: 'start',
+            color: 'inherit',
+            'aria-label': 'menu',
+            'aria-haspopup': 'true',
+            onClick: handleDrawerOpen
+          }}>
           <MenuIcon />
         </IconButton>
 
         <Drawer
           {...{
-            anchor: "left",
+            anchor: 'left',
             open: drawerOpen,
-            onClose: handleDrawerClose,
-          }}
-        >
+            onClose: handleDrawerClose
+          }}>
           <div className={drawerContainer}>{getDrawerChoices()}</div>
           <Button
             style={{
-              marginLeft: "-30px",
-              textTransform: "none",
-              fontFamily: "Open Sans, sans-serif",
+              marginLeft: '-30px',
+              textTransform: 'none',
+              fontFamily: 'Open Sans, sans-serif',
               fontWeight: 800,
-              size: "30px",
+              size: '30px'
             }}
-            onClick={LoginPopup}
-          >
+            onClick={LoginPopup}>
             Login
           </Button>
         </Drawer>
 
         <div>{femmecubatorLogo}</div>
       </Toolbar>
-    );
-  };
+    )
+  }
 
   const getDrawerChoices = () => {
-    const handleClick = (event) => {
+    /* const handleClick = (event) => {
       setAnchorEl(event.currentTarget);
     };
 
-    const handleClose = () => {
+  const handleClose = () => {
       setAnchorEl(null);
     };
 
@@ -219,56 +209,48 @@ export default function Header() {
       setOpenDialog("PRIVACY");
       handleClose();
     };
-
     const closeDialog = () => {
       setOpenDialog(null);
-    };
+    }; */
 
     return headersData.map(({ label, href }) => {
-      return label === "Login" ? (
-        <div>
-          <MenuItem onClick={openTermsDialog}>{label}</MenuItem>
-          <Terms open={openDialogName === "TERMS"} onClose={closeDialog} />
-        </div>
-      ) : (
+      return (
         <Link
+          key={label}
           {...{
             component: RouterLink,
             to: href,
-            color: "inherit",
-            style: { textDecoration: "none" },
-            key: label,
-          }}
-        >
+            color: 'inherit',
+            style: { textDecoration: 'none' }
+          }}>
           <MenuItem>{label}</MenuItem>
         </Link>
-      );
-    });
-  };
+      )
+    })
+  }
 
   const femmecubatorLogo = (
     <Typography variant="h6" component="h1" className={logo}>
       BookMyPG
     </Typography>
-  );
+  )
 
   const getMenuButtons = () => {
     return headersData.map(({ label, href }) => {
       return (
         <Button
+          key={label}
           {...{
-            key: label,
-            color: "inherit",
+            color: 'inherit',
             to: href,
             component: RouterLink,
-            className: menuButton,
-          }}
-        >
+            className: menuButton
+          }}>
           {label}
         </Button>
-      );
-    });
-  };
+      )
+    })
+  }
 
   return (
     <>
@@ -281,8 +263,7 @@ export default function Header() {
         <Dialog
           open={open}
           onClose={handleClose}
-          aria-labelledby="form-dialog-title"
-        >
+          aria-labelledby="form-dialog-title">
           <DialogTitle id="form-dialog-title">
             Please login to your account
           </DialogTitle>
@@ -290,10 +271,10 @@ export default function Header() {
             <GoogleLoginButton
               text="Sign In with Google"
               style={{
-                width: "230px",
-                height: "35px",
-                justifyContent: "center",
-                textAlign: "center",
+                width: '230px',
+                height: '35px',
+                justifyContent: 'center',
+                textAlign: 'center'
               }}
               onClick={handleLogin}
             />
@@ -301,13 +282,12 @@ export default function Header() {
               variant="contained"
               color="secondary"
               onClick={handleClose}
-              className={buttonmargin}
-            >
+              className={buttonmargin}>
               Cancel
             </Button>
           </div>
         </Dialog>
       </div>
     </>
-  );
+  )
 }
